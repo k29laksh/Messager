0c2ba677f4c2494eab42c3f911eb93971f70e547
k29laksh <klaxay0505@gmail.com>
2024-06-13 14:25:40 +0530

    next-auth

---
+++ src/types/ApiResponse.ts
@@ -0,0 +1,11 @@
+import { Message } from "@/models/user";
+
+export interface ApiResponse{
+
+    success:boolean;
+    message:string;
+
+    isAcceptingMessages?:boolean;
+    messages?: Array<Message>
+    
+}
